7edfd1be6dca36e3b940999f78fa93b804d067c1
lixiang <lixiang@builds-iMac.local>
2017-03-14 16:34:21 +0800

    aaaa

---
+++ src/lixiang/test.java
@@ -0,0 +1,19 @@
+package lixiang;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		
+		System.out.println("hello");
+		
+	}
+}
